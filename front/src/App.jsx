e47f5f6e6174ac22d2483209f55fda7c303bc077
Kariem <kariem@hvarstore.com>
2025-08-05 23:34:32 +0300

    Update .htaccess and frontend components

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,30 +1,45 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoadingScreen from './components/common/LoadingScreen';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import OrderManagementPage from './pages/OrderManagementPage';
+import NotFoundPage from './pages/NotFoundPage';
+import { Toaster } from 'react-hot-toast';
 
-// Lazy load pages
-const OrderManagementPage = lazy(() => import('./pages/OrderManagementPage'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
-
-const HvarHub = () => {
+function App() {
   return (
     <ThemeProvider initialTheme="dark" initialRTL={true}>
       <Router>
-        <Suspense fallback={<LoadingScreen />}>
-          <div dir="rtl" lang="ar">
-            <Routes>
-              {/* Main Routes */}
-              <Route path="/" element={<Navigate replace to="/order-management" />} />
-              <Route path="/order-management" element={<OrderManagementPage />} />
-              {/* Error Routes */}
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </Suspense>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+          <Routes>
+            <Route path="/" element={<OrderManagementPage />} />
+            <Route path="/orders" element={<OrderManagementPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            className: '',
+            duration: 4000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+            success: {
+              duration: 3000,
+              theme: {
+                primary: '#4aed88',
+              },
+            },
+          }}
+        />
       </Router>
     </ThemeProvider>
   );
-};
+}
 
-export default HvarHub;
+export default App;
--- front/src/components/common/index.js
+++ front/src/components/common/index.js
@@ -7,4 +7,11 @@ export { default as LoadingScreen } from './LoadingScreen';
 export { default as OrderCard } from './OrderCard';
 export { default as PermissionDialog } from './PermissionDialog';
 export { default as Tabs } from './Tabs';
-export { default as Timeline } from './Timeline'; 
+export { default as Timeline } from './Timeline';
+
+// Order components
+export { default as ScannerSection } from '../order/ScannerSection';
+export { default as OrderList } from '../order/OrderList';
+export { default as TabNavigation } from '../order/TabNavigation';
+export { default as RecentScans } from '../order/RecentScans';
+export { default as ConfirmationPanel } from '../order/ConfirmationPanel'; 
